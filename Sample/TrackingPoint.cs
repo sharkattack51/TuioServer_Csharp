f8b75909d2b0aa5f4f645bf404d2ce934dae5a31
Koichi Kasai <sharkattack51@gmail.com>
2016-08-13 23:24:08 +0900

    add sample code.

---
+++ Sample/TrackingPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using TUIO;
+
+class TrackingPoint
+{
+	public enum POINT_EVENT
+	{
+		DOWN = 0,
+		MOVE,
+		UP
+	}
+	
+	public POINT_EVENT ptEvent = POINT_EVENT.DOWN;
+	public bool isTracked = false;
+	public Point position = new Point();
+	public Point pre_position = new Point();
+
+	public UrgPoint()
+	{
+	
+	}
+}
---
+++ Sample/TuioSample.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TUIO;
+
+/**
+* TuioServer_C# sample code.
+* required TUIO11_NET libraries.
+* http://prdownloads.sourceforge.net/reactivision/TUIO11_NET-1.1.5.zip?download
+*/
+
+class TuioSample
+{
+	private TuioServer server;
+
+	
+	public TuioSample()
+	{
+		this.server = new TuioServer();
+	}
+
+	~TuioSample()
+	{
+		this.server.Close();
+	}
+
+
+	// enterframe function
+	public void ProcessTUIO(List<TrackingPoint> trackingPoints)
+	{
+		if(this.server == null)
+			return;
+
+		// init
+		this.server.initFrame(TuioTime.SessionTime);
+
+		// event process
+		foreach(TrackingPoint pt in trackingPoints)
+		{
+			switch(pt.ptEvent)
+			{
+				case UrgPoint.POINT_EVENT.DOWN:
+
+					// cursor add
+					TuioCursor tcur_new = this.server.addTuioCursor((float)pt.position.X, (float)pt.position.Y);
+					break;
+
+				case UrgPoint.POINT_EVENT.MOVE:
+
+					// cursor update
+					foreach (TuioCursor tcur_move in this.server.getTuioCursors())
+					{
+						if (tcur_move.getDistance((float)pt.pre_position.X, (float)pt.pre_position.Y) == 0.0)
+						{
+							this.server.updateTuioCursor(tcur_move, (float)pt.position.X, (float)pt.position.Y);
+							break;
+						}
+					}
+					break;
+
+				case UrgPoint.POINT_EVENT.UP:
+
+					// cursor remove
+					foreach (TuioCursor tcur_del in this.server.getTuioCursors())
+					{
+						if (tcur_del.getDistance((float)pt.position.X, (float)pt.position.Y) == 0.0f)
+						{
+							this.server.removeTuioCursor(tcur_del);
+							break;
+						}
+					}
+					break;
+
+				default: break;
+			}
+		}
+
+		// commit
+		this.server.stopUntouchedMovingCursors();
+		this.server.removeUntouchedStoppedCursors();
+		this.server.commitFrame();
+	}
+
+	public void ClearTUIO()
+	{
+		this.server.Clear();
+	}
+
+	public List<TuioCursor> GetCursors()
+	{
+		return this.server.getTuioCursors();
+	}
+}
